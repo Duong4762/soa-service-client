cfadeb0c1faaa713125cd3f20a2aaf9fd57699bf
duydong0201 <duydong02012004@gmail.com>
2026-03-11 05:31:44 +0700

    add publisher2 client log

--- src/main/java/org/example/serviceclient/client/Publisher2FeignClient.java
+++ src/main/java/org/example/serviceclient/client/Publisher2FeignClient.java
@@ -1,7 +1,11 @@
 package org.example.serviceclient.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "service-publisher-2")
+@FeignClient(name = "service-publisher-2", path = "/")
 public interface Publisher2FeignClient {
+    @PostMapping("/api/test")
+    String callTestEndpoint(@RequestBody String request);
 }
--- src/main/java/org/example/serviceclient/controller/TestController.java
+++ src/main/java/org/example/serviceclient/controller/TestController.java
@@ -3,6 +3,7 @@ package org.example.serviceclient.controller;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.example.serviceclient.client.Publisher1FeignClient;
+import org.example.serviceclient.client.Publisher1FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TestController {
     private final Publisher1FeignClient publisher1FeignClient;
+    private final Publisher1FeignClient publisher2FeignClient;
 
     @PostMapping("/call-publisher-1")
     public void test(@RequestBody String request){
         publisher1FeignClient.callTestEndpoint(request);
+    }
+
+    @PostMapping("/call-publisher-2")
+    public void callPublisher2(@RequestBody String request){
+        publisher2FeignClient.callTestEndpoint(request);
     }
 }
